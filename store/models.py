964f9139dd800707e61a61426139450c073b6656
Ramesh777 <me@example.com>
2026-02-25 14:29:48 +0530

    Switched to CloudinaryField

--- store/models.py
+++ store/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="products/", null=True, blank=True)
+    image = CloudinaryField("image", null=True, blank=True)
     stock = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
